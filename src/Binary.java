6c56256782464e0da5616df9baff7cdae2ce69e9
crysis74 <jekabt2@mail.ru>
2017-12-23 18:38:47 +0300

    er

--- src/Binary.java
+++ src/Binary.java
@@ -1,10 +1,11 @@
-public abstract class Binary extends Unary {
+public abstract class Binary  {
 
+    protected Expression per1;
     protected Expression per2;
 
     public Binary(Expression per1, Expression per2) {
 
-        super(per1);
+        this.per1 = per1;
         this.per2 = per2;
     }
     public abstract int evaluate(int x, int y, int z) throws ArithmeticExceptoin;
